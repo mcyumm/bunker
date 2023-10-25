d14254a56e7ef954593e1fffb189203f66252c2c
mcyumm <mcyumm@ya.ru>
2023-10-25 11:01:17 +0300

    Добавил описания бункера и катастрофы + поменял название генерируемых файлов

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import random
 x = int(input())
+y = random.randint(0, 6)
+disaster = random.choice(open('disaster.txt').readlines())
+bunker = (open(f'Бункер{y}.txt'))
+h = open('Катастрофа.txt', 'w')
+h.write(disaster)
+h.close()
 for i in range(x):
 
 
     sex = random.randint(0,1)
     age = random.randint(14,100)
-    cf = random.randint(0,4)
+    cf = random.randint(0,6)
     weight = random.choice(open('weight.txt').readlines())
     height = str(random.randint(155,199))
     ht = random.choice(open('htrait.txt').readlines())
@@ -17,7 +23,7 @@ for i in range(x):
     item = random.choice(open('item.txt').readlines())
 
 
-    f = open(f'player{i+1}.txt', 'w')
+    f = open(f'Игрок {i+1}.txt', 'w')
     if sex == 1:
         f.write(f'Пол: Мужской, {str(age)}')
     else:
